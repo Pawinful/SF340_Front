6d0128a4ca6c77f33de4bb13520f1a50fba43f0d
Poompu Srimanut <pumipu@MacBook-Air-khxngPoompu.local>
2025-03-05 14:15:36 +0700

    api

--- src/components/pages/user/Home.jsx
+++ src/components/pages/user/Home.jsx
@@ -1,92 +1,81 @@
-import React from 'react';
-import { FaMapMarkerAlt, FaHome } from 'react-icons/fa';
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { FaMapMarkerAlt, FaHome } from "react-icons/fa";
 import { RiStairsFill } from "react-icons/ri";
 import { HiUsers } from "react-icons/hi2";
 
-const rooms = [
-    {
-        id: 1,
-        name: 'Meeting Room 1',
-        location: 'Rangsit Campus',
-        space: 'Faculty of Engineering, Library',
-        floor: 'Fl.2',
-        capacity: '4 - 6',
-    },
-    {
-        id: 2,
-        name: 'Meeting Room 2',
-        location: 'Rangsit Campus',
-        space: 'Faculty of Engineering, Library ',
-        floor: 'Fl.2',
-        capacity: '1 - 2',
-    },
-    {
-        id: 3,
-        name: 'Meeting Room 3',
-        location: 'Rangsit Campus',
-        space: 'Faculty of Engineering, Library',
-        floor: 'Fl.2',
-        capacity: '1 - 2',
-    },
-    {
-        id: 3,
-        name: 'Meeting Room 4',
-        location: 'Rangsit Campus',
-        space: 'Faculty of Engineering, Library',
-        floor: 'Fl.2',
-        capacity: '1 - 2',
-    },
-    {
-        id: 3,
-        name: 'Meeting Room 5',
-        location: 'Rangsit Campus',
-        space: 'Faculty of Engineering, Library',
-        floor: 'Fl.2',
-        capacity: '1 - 2',
-    },
-];
+const baseURL = "http://localhost:3000/api/rooms/getAllRoom";
 
-const RoomCard = ({ room }) => {
-    return (
-        <div className='bg-white shadow-md rounded-lg p-6 mb-6 flex flex-col gap-4'>
-            <div className='flex items-center gap-6'>
-                <div className='bg-gray-200 p-6 rounded-lg'>
-                    <FaHome className='text-grey-600 text-5xl' />
-                </div>
-                <div>
-                    <h2 className='text-lg font-semibold mb-2'>{room.name}</h2>
-                    <p className='text-sm text-gray-500 flex items-center gap-2'>
-                        <FaMapMarkerAlt /> {room.location}
-                    </p>
-                    <p className='text-sm text-gray-500 flex items-center gap-2'>
-                        <FaHome /> {room.space}
-                    </p>
-                    <p className='text-sm text-gray-500 flex items-center gap-2'>
-                        <RiStairsFill /> {room.floor}
-                    </p>
-                </div>
-            </div>
-
-            <div className='flex justify-between items-center'>
-                <div className='flex items-center gap-2 '><HiUsers /> {room.capacity} </div>
-                <button className='bg-[#C53739] text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-[#8A2A2B]'>จองเลย</button>
-            </div>
+const RoomCard = ({ post }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-6 mb-6 flex flex-col gap-4">
+      {/* <h1>{post.data.seat}</h1> */}
+      <div className="flex items-center gap-6">
+        <div className="bg-gray-200 p-6 rounded-lg">
+          {/* <FaHome className='text-grey-600 text-5xl' /> */}
+          <img src="" alt="" />
         </div>
-    )
-}
+        <div>
+          <h2 className="text-lg font-semibold mb-2">{post.roomNameEN}</h2>
+          <p className="text-sm text-gray-500 flex items-center gap-2">
+            <FaMapMarkerAlt /> {post.branch}
+          </p>
+          <p className="text-sm text-gray-500 flex items-center gap-2">
+            <FaHome /> Library
+          </p>
+          <p className="text-sm text-gray-500 flex items-center gap-2">
+            <RiStairsFill /> {post.building}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2 ">
+          <HiUsers /> {post.seat}{" "}
+        </div>
+        <button className="bg-[#C53739] text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-[#8A2A2B]">
+          จองเลย
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const Home = () => {
-    return (
-        <>
-            <div className='bg-[#EBEDF1] min-h-screen'>
-                <div className='p-6 max-w-4xl mx-auto'>
-                    {rooms.map((room) => (
-                        <RoomCard key={room.id} room={room} />
-                    ))}
-                </div>
-            </div>
-        </>
-    )
-}
+  //   const [post, setPost] = useState(null);
+  //   useEffect(() => {
+  //     axios.get(baseURL).then((response) => {
+  //       setPost(response.data);
+  //       console.log(response.data);
+  //     });
+  //   }, []);
+  //   if (!post) return null;
+
+  const getData = () => {
+    axios
+      .get(baseURL)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div className="bg-[#EBEDF1] min-h-screen">
+        <div className="p-6 max-w-4xl mx-auto">
+          <button onClick={getData}>Get Data</button>
+          {/* <p>{post.data[0].roomNameEN}</p>
+          {post.map((post) => (
+            <RoomCard key={post._id} post={post} />
+          ))} */}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
