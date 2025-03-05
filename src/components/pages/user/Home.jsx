69cb1ebc0f2a99cff8e02cbf70fe4b20350b4e19
Poompu Srimanut <pumipu@MacBook-Air-khxngPoompu.local>
2025-03-05 16:27:48 +0700

    api connect

--- src/components/pages/user/Home.jsx
+++ src/components/pages/user/Home.jsx
@@ -7,7 +7,7 @@ import { HiUsers } from "react-icons/hi2";
 
 const baseURL = "http://localhost:3000/api/rooms/getAllRoom";
 
-const RoomCard = ({ post }) => {
+const RoomCard = ({ room }) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-6 mb-6 flex flex-col gap-4">
       {/* <h1>{post.data.seat}</h1> */}
@@ -17,22 +17,22 @@ const RoomCard = ({ post }) => {
           <img src="" alt="" />
         </div>
         <div>
-          <h2 className="text-lg font-semibold mb-2">{post.roomNameEN}</h2>
+          <h2 className="text-lg font-semibold mb-2">{room.roomNameEN}</h2>
           <p className="text-sm text-gray-500 flex items-center gap-2">
-            <FaMapMarkerAlt /> {post.branch}
+            <FaMapMarkerAlt /> {room.branch}
           </p>
           <p className="text-sm text-gray-500 flex items-center gap-2">
-            <FaHome /> Library
+            <FaHome /> {room.roomType}
           </p>
           <p className="text-sm text-gray-500 flex items-center gap-2">
-            <RiStairsFill /> {post.building}
+            <RiStairsFill /> {room.building}
           </p>
         </div>
       </div>
 
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-2 ">
-          <HiUsers /> {post.seat}{" "}
+          <HiUsers /> {room.seat}
         </div>
         <button className="bg-[#C53739] text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-[#8A2A2B]">
           จองเลย
@@ -43,35 +43,24 @@ const RoomCard = ({ post }) => {
 };
 
 const Home = () => {
-  //   const [post, setPost] = useState(null);
-  //   useEffect(() => {
-  //     axios.get(baseURL).then((response) => {
-  //       setPost(response.data);
-  //       console.log(response.data);
-  //     });
-  //   }, []);
-  //   if (!post) return null;
+  const [post, setPost] = useState(null);
 
-  const getData = () => {
-    axios
-      .get(baseURL)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+      console.log(response.data.data);
+    });
+  }, []);
+
+  if (!post) return null;
 
   return (
     <>
       <div className="bg-[#EBEDF1] min-h-screen">
         <div className="p-6 max-w-4xl mx-auto">
-          <button onClick={getData}>Get Data</button>
-          {/* <p>{post.data[0].roomNameEN}</p>
-          {post.map((post) => (
-            <RoomCard key={post._id} post={post} />
-          ))} */}
+          {post.data.map((room) => (
+            <RoomCard key={room.id} room={room} />
+          ))}
         </div>
       </div>
     </>
--- src/components/pages/user/UserLogin.jsx
+++ src/components/pages/user/UserLogin.jsx
@@ -1,41 +1,105 @@
 import React from "react";
 import logo from "../../Assets/TSE_LOGO.png";
+import { useRef, useState, useEffect } from "react";
 
 function Login() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
+    setSuccess(true);
+  };
+
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-white p-6">
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to home</a>
+          </p>
+        </section>
+      ) : (
+        <section
+          className="h-screen flex flex-col items-center justify-center bg-white p-6"
+          onSubmit={handleSubmit}
+        >
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
 
-      <div className="absolute top-1">
-        <img className="w-150" src={logo} alt="TSE Logo" />
-      </div>
+          <div className="absolute top-1">
+            <img className="w-150" src={logo} alt="TSE Logo" />
+          </div>
 
-      <div className="flex flex-col items-center mt-15">
-        <h2 className="text-4xl font-bold text-gray-800 mb-15">TSE Meeting Room</h2>
-        <p className="text-xl font-semibold text-gray-600 ">Sign in</p>
+          <div className="flex flex-col items-center mt-15">
+            <h2 className="text-4xl font-bold text-gray-800 mb-15">
+              TSE Meeting Room
+            </h2>
+            <p className="text-xl font-semibold text-gray-600 ">Sign in</p>
 
-        <div className="mt-10 w-96">
-          <label className="block text-gray-700 font-medium mb-4">TSE ID</label>
-          <input
-            type="text"
-            className="w-full p-3.5 bg-[#EBEDF1] rounded-md  focus:outline-none"
-          />
+            <form className="mt-10 w-96">
+              <label className="block text-gray-700 font-medium mb-4">
+                TSE ID
+              </label>
+              <input
+                type="text"
+                className="w-full p-3.5 bg-[#EBEDF1] rounded-md  focus:outline-none"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
 
-          <label className="block text-gray-700 font-medium mt-8 mb-4">Password</label>
-          <input
-            type="password"
-            className="w-full p-3.5 bg-[#EBEDF1] rounded-md  focus:outline-none"
-          />
-        </div>
+              <label className="block text-gray-700 font-medium mt-8 mb-4">
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full p-3.5 bg-[#EBEDF1] rounded-md  focus:outline-none"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+              />
+            </form>
 
-        <div className="mt-15 text-center">
-          <p className="text-sm text-gray-600 mb-2">จองล่วงหน้าอย่างน้อย 1 วัน</p>
-          <button className="w-30 bg-[#8A2A2B] text-white py-1.5 rounded-md text-lg font-semibold hover:bg-[#621d1e] cursor-pointer">
-            Sign In
-          </button>
-        </div>
-      </div>
-    </div>
-  )
+            <div className="mt-15 text-center">
+              <p className="text-sm text-gray-600 mb-2">
+                จองล่วงหน้าอย่างน้อย 1 วัน
+              </p>
+              <button className="w-30 bg-[#8A2A2B] text-white py-1.5 rounded-md text-lg font-semibold hover:bg-[#621d1e] cursor-pointer">
+                Sign In
+              </button>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
 }
 
 export default Login;
